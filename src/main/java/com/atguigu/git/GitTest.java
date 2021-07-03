dfde4a5d837705aa9ff421139d440567b3110335
lzw <543606467@qq.com>
2021-07-03 18:50:37 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author lzw
+ * @create 2021-07-03 18:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
